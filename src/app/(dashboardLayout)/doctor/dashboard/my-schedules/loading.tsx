56f6683c716e7c27350bb396cab4d3b04212967f
rasel754 <raselahmed73614@gmail.com>
2026-08-27 23:27:19 +0600

    download all component

---
+++ src/app/(dashboardLayout)/doctor/dashboard/my-schedules/loading.tsx
@@ -0,0 +1,4 @@
+export default function DoctorSchedulesLoading() {
+  // Or a custom loading skeleton component
+  return <p>Doctor Schedules Loading...</p>;
+}
---
+++ src/app/(dashboardLayout)/doctor/dashboard/prescriptions/loading.tsx
@@ -0,0 +1,4 @@
+export default function DoctorDashboardPrescriptionsLoading() {
+  // Or a custom loading skeleton component
+  return <p>Doctor Dashboard Prescriptions Loading...</p>;
+}
